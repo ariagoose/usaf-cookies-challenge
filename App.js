9a7512d9a8ccb5ef568a28a0078eb044d67765c5
ariagoose <73117176+ariagoose@users.noreply.github.com>
2020-11-12 10:25:46 -0500

    cleaned up

--- App.js
+++ App.js
@@ -29,24 +29,6 @@ app.post('/login', (req, res) => {
     res.end();
   })
 
-// app.get('/users', (req, res) => res.send(users))
-
-// app.get('/users/:id', (req, res) => {
-//     res.send(users.find(person => person.id === parseInt(req.params.id)));
-// })
-
-// app.post('/users', (req, res) => {
-//     /* POST user data using the request body */
-//     let newUser = req.body;
-//     users.push(newUser);
-//     res.send(newUser);
-// })
-
-// app.get('/search', (req, res) => {
-//     let nameArray = req.query.name.split(" ");
-    
-//     res.send(users.filter(person => ((person.name[0] === nameArray[0]) && (person.name[1] === nameArray[1]))));
-// })
 
 const port = 3000
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
